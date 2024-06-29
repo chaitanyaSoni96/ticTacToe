f7d6b003a53d416831de0d053e283190537ce5f8
Chaitanya Soni <sonichaitanya13@gmail.com>
2024-06-29 11:07:06 +0530

    create project

---
+++ ticTacToe/ticTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ticTacToe
+//
+//  Created by Chaitanya Soni on 29/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
